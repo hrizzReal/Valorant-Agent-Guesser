965b147131e49ccae603f208faf7756b9665475c
hrizzReal <95167819+hrizzReal@users.noreply.github.com>
2024-12-14 17:29:15 +0530

    js added

---
+++ js/words.js
@@ -0,0 +1,102 @@
+let wordList = [
+    {
+        word: "gekko",
+        hint: "slimy blobs"
+    },
+    {
+        word: "killjoy",
+        hint: "nerd ahh"
+    },
+    {
+        word: "yoru",
+        hint: "cool blue boy"
+    },
+    {
+        word: "reyna",
+        hint: "euhh hairline"
+    },
+    {
+        word: "chamber",
+        hint: "simp"
+    },
+    {
+        word: "brimstone",
+        hint: "father figure"
+    },
+    {
+        word: "breach",
+        hint: "angry man"
+    },
+    {
+        word: "viper",
+        hint: "snakes"
+    },
+    {
+        word: "phoenix",
+        hint: "fire boy"
+    },
+    {
+        word: "sage",
+        hint: "doctor"
+    },
+    {
+        word: "sova",
+        hint: "russian "
+    },
+    {
+        word: "cypher",
+        hint: "cameraman"
+    },
+    {
+        word: "omen",
+        hint: "cute uwu shadow"
+    },
+    {
+        word: "jett",
+        hint: "rebibe me ____"
+    },
+    {
+        word: "raze",
+        hint: "boom n paint"
+    },
+    {
+        word: "skye",
+        hint: "environmentalist"
+    },
+    {
+        word: "astra",
+        hint: "outer space"
+    },
+    {
+        word: "kay/o",
+        hint: "cool ahh robot"
+    },
+    {
+        word: "neon",
+        hint: "fast girl"
+    },
+    {
+        word: "fade",
+        hint: "goth woman"
+    },
+    {
+        word: "harbor",
+        hint: "ocean man"
+    },
+    {
+        word: "deadlock",
+        hint: "nets and cages"
+    },
+    {
+        word: "iso",
+        hint: "forcefield boy"
+    },
+    {
+        word: "clove",
+        hint: "butterfly girl"
+    },
+    {
+        word: "vyse",
+        hint: "pure metal"
+    }
+]
